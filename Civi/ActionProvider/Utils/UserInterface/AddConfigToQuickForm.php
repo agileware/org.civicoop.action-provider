4efcbdf1f93710285ec57c5057eeec81d31c81fb
Jaap Jansma <jaap@edeveloper.nl>
2018-02-09 18:38:50 +0100

    added layouts for entity ref and select.

--- Civi/ActionProvider/Utils/UserInterface/AddConfigToQuickForm.php
+++ Civi/ActionProvider/Utils/UserInterface/AddConfigToQuickForm.php
@@ -4,6 +4,8 @@ namespace Civi\ActionProvider\Utils\UserInterface;
 
 use \Civi\ActionProvider\Action\AbstractAction;
 
+use CRM_ActionProvider_ExtensionUtil as E;
+
 /**
  * Helper class to add a configuration specification on to a quick form class.
  */
@@ -28,13 +30,32 @@ class AddConfigToQuickForm {
 		$prefix = $action->getName();
 		$elementNames[$prefix] = array();
 		foreach($action->getConfigurationSpecification() as $config_field) {
+			$field_name = $prefix.$config_field->getName();
 			$attributes = array(
 				'class' => $prefix,
 			);	
 			
-			if ($config_field->getDataType() == 'Integer') {
-				$form->add('text', $prefix.$config_field->getName(), $config_field->getTitle(), $attributes, $config_field->isRequired());
-				$elementNames[$prefix][] = $prefix.$config_field->getName();
+			if (!empty($config_field->getFkEntity())) {
+				$attributes['entity'] = $config_field->getFkEntity();
+				if ($config_field->isMultiple()) {
+					$attributes['multiple'] = true;
+				}
+				$form->addEntityRef($field_name, $config_field->getTitle(), $attributes, $config_field->isRequired());
+				$elementNames[$prefix][] = $field_name;
+			}else if (!empty($config_field->getOptions())) {
+				$attributes['class'] .= ' crm-select2 huge';
+				if ($config_field->isMultiple()) {
+					$attributes['multiple'] = true;
+					$options = $config_field->getOptions();
+				} else {
+					$options = array(E::ts(' - select -')) + $config_field->getOptions();
+				}
+				$form->add('select', $field_name, $config_field->getTitle(), $options, $config_field->isRequired(), $attributes);
+				$elementNames[$prefix][] = $field_name;
+			} else {
+				$attributes['class'] .= ' huge';
+				$form->add('text', $field_name, $config_field->getTitle(), $attributes, $config_field->isRequired());
+				$elementNames[$prefix][] = $field_name;
 			}
 		}
 		$form->assign('actionProviderElementNames', $elementNames);
