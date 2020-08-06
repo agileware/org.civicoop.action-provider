c8b688ff551ca816905f96f700792e00847a7365
Dev <dev@debian.Debian>
2020-08-06 12:29:20 +0200

    Adding action to update custom data for civicase

---
+++ Civi/ActionProvider/Action/CiviCase/UpdateCustomData.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Civi\ActionProvider\Action\CiviCase;
+
+use \Civi\ActionProvider\Action\AbstractAction;
+use \Civi\ActionProvider\Parameter\ParameterBagInterface;
+use \Civi\ActionProvider\Parameter\SpecificationBag;
+use \Civi\ActionProvider\Parameter\Specification;
+use \Civi\ActionProvider\Utils\CustomField;
+
+use CRM_ActionProvider_ExtensionUtil as E;
+
+class UpdateCustomData extends AbstractAction {
+  
+  /**
+   * Run the action
+   * 
+   * @param ParameterInterface $parameters
+   *   The parameters to this action.
+   * @param ParameterBagInterface $output
+   *   The parameters this action can send back 
+   * @return void
+   */
+  protected function doAction(ParameterBagInterface $parameters, ParameterBagInterface $output) {
+    $apiParams['id'] = $parameters->getParameter('case_id');
+    foreach($this->getParameterSpecification() as $spec) {
+      if (stripos($spec->getName(), 'custom_')!==0) {
+        continue;
+      }
+      if ($parameters->doesParameterExists($spec->getName())) {
+        $apiParams[$spec->getApiFieldName()] = $parameters->getParameter($spec->getName());
+      }
+    }
+    $result = civicrm_api3('Case', 'create', $apiParams);
+  }
+  
+  /**
+   * Returns the specification of the configuration options for the actual action.
+   * 
+   * @return SpecificationBag
+   */
+  public function getConfigurationSpecification() {
+    return new SpecificationBag();
+  }
+  
+  /**
+   * Returns the specification of the parameters of the actual action.
+   * 
+   * @return SpecificationBag
+   */
+  public function getParameterSpecification() {
+    $specs = new SpecificationBag();
+    $specs->addSpecification(new Specification('case_id', 'Integer', E::ts('Case ID'), true));
+    
+    $customGroups = civicrm_api3('CustomGroup', 'get', array('is_active' => 1, 'is_multiple' => 0, 'options' => array('limit' => 0)));
+    foreach($customGroups['values'] as $customGroup) {
+      if (!in_array($customGroup['extends'], array('Case'))) {
+        continue;
+      }
+      
+      $customFields = civicrm_api3('CustomField', 'get', array('custom_group_id' => $customGroup['id'], 'is_active' => 1, 'options' => array('limit' => 0)));
+      foreach($customFields['values'] as $customField) {
+        if (isset($customField['is_view']) && $customField['is_view']) {
+          continue;
+        }
+
+        $spec = CustomField::getSpecFromCustomField($customField, $customGroup['title'].': ', false);
+        if ($spec) {
+          $specs->addSpecification($spec);
+        }
+      }
+    }
+    return $specs;
+  }
+  
+  /**
+   * Returns the specification of the output parameters of this action.
+   * 
+   * This function could be overriden by child classes.
+   * 
+   * @return SpecificationBag
+   */
+  public function getOutputSpecification() {
+    return new SpecificationBag();
+  }
+    
+  
+}
